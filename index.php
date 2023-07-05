8e41771a9fe860329e3f065e32bfd21151fe2891
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-07-05 17:45:33 +0200

    [STYLE] stilizzazione pagine

--- index.php
+++ index.php
@@ -4,24 +4,59 @@
 
 ?>
 
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Password Generator</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
     <body>
         <div>
-            <form action="showPassword.php" method="get">
-                <label for="password">Lunghezza password</label>
-                <input type="number" id="password" name="passwordLength">
-                <button type="submit">Invia</button>
-            </form>
-        </div>
+            <header>
+                <div class="container">
+                    <div class="row">
+                        <div class="col ">
+                            <div class="text-center pt-3">
+                                <h1>
+                                    STRONG PASSWORD GENERATOR
+                                </h1>
+                                <h2>
+                                    Crea la tua password sicura!
+                                </h2>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </header>
+            <main >
+            <div class="container bg-light">
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <h5 class="mt-3">
+                            <?php echo isset($_GET['message']) ? $_GET['message'] : ''; ?>
+            
+                        </h5>
+                        <div class="form-group p-5">
+                            <form action="showPassword.php" class="form-control d-flex justify-content-around" method="get">
+                                <label for="password">Lunghezza password</label>
+                                <input class="form-control w-25" type="number" id="password" name="passwordLength">
+                                <button class="btn btn-primary" type="submit">Invia</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            </div>
+
+            </main>
         <div>
         
         </div>
--- showPassword.php
+++ showPassword.php
@@ -7,7 +7,7 @@
         $password = generatePassword();
         // metto una condizione se non si inserisca un numero
     } elseif ($_GET['passwordLength'] == '') {
-        header('location: ./index.php');
+        header('location: ./index.php?message=Scegli un parametro');
     }
 ?>
 
@@ -19,14 +19,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
     <body>
-    <?php if(isset($password) && $password != ''){ ?>
-        <div>
-            <?php echo$password ?>
+        <div class="contenier">
+            <div class="row justify-content-center">
+                <div class=" col-8 text-center">
+                    <div class="mt-5 p-3 bg-light">
+                        <div>
+                            Ecco la tua password:
+                        </div>
+                        <?php if(isset($password) && $password != ''){ ?>
+                            <h3>
+                                <?php echo$password ?>
+                        </h3>
+                        <div>
+                            Non mostrare e non condividere con nessuno la password!
+                        </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <?php } ?>
     </body>
 </html>
 
